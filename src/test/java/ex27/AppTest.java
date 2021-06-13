e49e8bfd098a6b1becc192bf5705917d9a2a821e
Colin <benhawk6003@gmail.com>
2021-06-13 14:47:29 -0400

    exercise 27

--- src/test/java/ex27/AppTest.java
+++ src/test/java/ex27/AppTest.java
@@ -2,11 +2,24 @@ package ex27;
 
 import org.junit.jupiter.api.Test;
 
+import static ex27.App.validateName;
+import static ex27.App.validateEmployeeID;
+import static ex27.App.validateZIPCode;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest
 {
     @Test
-    void main() {
+    void testvalidateName() {
+        assertEquals(0, validateName("John"));
+    }
+    @Test
+    void testvalidateEmployeeID() {
+        assertEquals(0, validateEmployeeID("AA-1234"));
+    }
+    @Test
+    void testvalidateZIP() {
+        assertEquals(0, validateZIPCode("55555"));
     }
 }
